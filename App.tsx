5d55a56f958d879f157065d3d3e9a53c3a6532e8
Matt Romage <ma33r@Matts-MacBook-Pro.local>
2023-11-24 21:43:46 -0500

    Created pages folder for app structuring

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './app/screens/Login';
+import Login from './screens/Login';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
