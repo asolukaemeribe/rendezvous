5d9f7c5eb07f5c959dd828772c96e04857cefc18
Matt Romage <ma33r@matts-mbp.mynetworksettings.com>
2023-11-29 01:01:57 -0500

    created backend server to query aws rds

---
+++ server/server.ts
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const serverConfig = require('./config')
+const routes = require('./routes.ts');
+
+const app = express();
+app.use(cors({
+    origin: '*',
+}));
+
+// We use express to define our various API endpoints and
+// provide their handlers that we implemented in routes.js
+app.get('/user/:uid', routes.user);
+
+app.get('/', (req, res) => {
+    res.send('Rendezvous Server made with Express');
+});
+
+app.listen(serverConfig.server_port, () => {
+    console.log(`Server running at http://${serverConfig.server_host}:${serverConfig.server_port}/`)
+});
