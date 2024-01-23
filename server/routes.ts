bc7c38f4c02fb6d7aa2460e2b9e0a9c1c131938b
Matt Romage <ma33r@lev-10965.apn.wlan.private.upenn.edu>
2024-01-22 21:27:03 -0500

    Added postgresql connection for managing user location data. Also added routes for adding new users, updating user location and getting users in a certain radius (in meters)

--- server/routes.ts
+++ server/routes.ts
@@ -1,18 +1,30 @@
+// connect to mysql user-schema database
 const mysql = require('mysql')
 const config = require('./config.json')
 
 // Creates MySQL connection using database credential provided in config.json
 // Do not edit. If the connection fails, make sure to check that config.json is filled out correctly
 const connection = mysql.createConnection({
-  host: config.rds_host,
-  user: config.rds_user,
-  password: config.rds_password,
-  port: config.rds_port,
-  database: config.rds_db
+  host: config.mysql_host,
+  user: config.mysql_user,
+  password: config.mysql_password,
+  port: config.mysql_port,
+  database: config.mysql_db
 });
 connection.connect((err) => err && console.log(err));
 
-// Routes
+// connect to postgres user-location database
+const pgp = require('pg-promise')(/* options */)
+const pql_db = pgp({
+    host: config.psql_host,
+    user: config.psql_user,
+    password: config.psql_password,
+    port: config.psql_port,
+    database: config.psql_db
+  })
+
+
+// mysql user-schema Routes
 const user = async function(req, res) {
     const userID = req.params.uid
 
@@ -52,6 +64,49 @@ const createuser = async function(req, res) {
             console.log("Success! New user created with id: " + uid);
         }
     });
+}
+
+// pql user-location Routes
+const createuserlocation = async function(req, res) {
+    const uid = req.query.uid
+    const lat = req.query.lat
+    const long = req.query.lat
+
+    pql_db.none(`INSERT INTO users (id, location) VALUES (${uid}, ST_GeomFromText('POINT(${long} ${lat})', 4326))`)
+    .catch((error) => {
+    console.log('ERROR:', error)
+    })
+}
+
+const getusersinradius = async function(req, res) {
+    const uid = req.query.uid
+    const lat = req.query.lat
+    const long = req.query.lat
+    const rad = req.query.rad
+
+    pql_db.any(`SELECT *
+    FROM users
+    WHERE ST_DWithin(location, 'POINT(${long} ${lat})', ${rad})
+        AND id != '${uid}'`)
+    .then(data => {
+        res.json(data); // print new user id;
+    })
+    .catch((error) => {
+        console.log('ERROR:', error)
+    })
+}
+
+const updateuserlocation = async function(req, res) {
+    const uid = req.query.uid
+    const lat = req.query.lat
+    const long = req.query.lat
+
+    pql_db.none(`UPDATE users
+    SET location = ST_GeomFromText('POINT(${long} ${lat})', 4326)
+    WHERE id = '${uid}'`)
+    .catch((error) => {
+        console.log('ERROR:', error)
+    })
 }
 
 module.exports = {
